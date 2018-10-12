213a1468ef7a560a9c1e7b6d160a0eea75d5a9c1
Michael Standen <screaminghawk@gmail.com>
2018-10-12 14:29:19 +1300

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # aot-youtube
-Always on top Youtube app generator
+
+Creates a native electron app linking to YouTube.com
+
+This app will display over the top of your other windows even when focus is lost.
+
+## Usage
+
+Install dependencies
+
+`npm i`
+
+Build it
+
+`npm run build`
+
+Run it
+
+`./build/YouTube`
+
+## Notes
+
+After building, you can copy the contents of the `build` folder into a convenient location for you.
---
+++ index.js
@@ -0,0 +1,85 @@
+var nativefier = require('nativefier').default;
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+var async = require('async');
+
+var buildFolder = 'build';
+var builtPath = '';
+var newPathEnd = 'youtube-app';
+var newPath = path.join(buildFolder, newPathEnd);
+
+function clearBuildFolder(callback){
+	rimraf(buildFolder, function(err){
+		if (err){
+			console.log('Unable to clear build folder');
+			console.log(err);
+			return;
+		}
+		callback();
+	});
+}
+
+function nativefy(callback){
+	// Config
+	var options = {
+		// Youtube URL
+		targetUrl: 'https://www.youtube.com',
+		out: buildFolder,
+		// No source
+		asar: false,
+		// Display and quality of life
+		singleInstance: true,
+		alwaysOnTop: true,
+		// Allow app to navigate
+		internalUrls: '*',
+		// Additional config
+		inject: [
+			"src/inject.css",
+		],
+	};
+
+	// Run it
+	nativefier(options, function(error, appPath) {
+		builtPath = appPath;
+		if (error) {
+			console.error(error);
+			return;
+		}
+		callback();
+	});
+}
+
+function renameBuildFolder(callback){
+	// Rename folder
+	fs.rename(builtPath, newPath, function(err){
+		if (err){
+			console.log('Unable to rename folder');
+			console.log(err);
+			return;
+		}
+		callback();
+	});
+}
+
+function createSymLink(callback){
+	// Create sym link
+	fs.symlink(path.join(newPathEnd, 'YouTube.exe'), path.join(buildFolder, 'YouTube'), function(err){
+		if (err){
+			console.log('Unable to create symbolic link');
+			console.log(err);
+			return;
+		}
+		callback();
+	});
+}
+
+// Run these things
+async.waterfall([
+			clearBuildFolder,
+			nativefy,
+			renameBuildFolder,
+			createSymLink
+		], function(){
+	console.log('App created successfully')
+})
